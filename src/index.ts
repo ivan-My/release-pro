5d82329d1a98a36ac6fc13cbb4dc94b0e01aba91
ivan-My <252511374@qq.com>
2023-05-30 21:01:21 +0800

    444

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-//import { exec } from './utils'
+
 import { execSync } from 'child_process'
 import colors from 'colors'
+import { readJSON } from './utils'
 
 
 // 检查工作目录状态
-function checkWorkingDirectoryStatus() {
+const checkWorkingDirectoryStatus = () => {
   try {
     execSync('git diff-index --quiet HEAD --')
   } catch (error) {
-    console.error( colors.red('工作目录有未提交的更改，请先提交或丢弃这些更改'))
+    console.error(colors.red('工作目录有未提交的更改，请先提交或丢弃这些更改'))
     process.exit(-1)
   }
 }
 
+
+
+const s =  execSync('git status --short --untracked-files=no')
+console.log(s.toString());
+
+
+
+const { version } = readJSON(`${process.cwd()}/package.json`);
+console.log(version);
 
 checkWorkingDirectoryStatus()
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,10 @@
 import { execSync } from 'child_process'
 import { promisify } from 'util'
+import fs from 'fs'
 
 export const exec = promisify(execSync);
+
+
+
+
+export const readJSON = file => JSON.parse(fs.readFileSync(file, 'utf8'));
