7268a05fed48381924ac8c106c022b4aad2902ea
ivan-My <252511374@qq.com>
2023-05-30 11:27:59 +0800

    22

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import { exec } from './utils'
+//import { exec } from './utils'
+import { execSync } from 'child_process'
+import colors from 'colors'
+
 
 // 检查工作目录状态
 function checkWorkingDirectoryStatus() {
   try {
-    // 执行 Git 命令检查工作目录状态
-   // exec('git diff-index --quiet HEAD --')
-    // 工作目录干净，没有未提交的更改
-    console.log('工作目录是干净的，没有未提交的更改。')
+    execSync('git diff-index --quiet HEAD --')
   } catch (error) {
-    // 工作目录有未提交的更改
-    console.error('工作目录有未提交的更改，请先提交或丢弃这些更改。')
+    console.error( colors.red('工作目录有未提交的更改，请先提交或丢弃这些更改'))
     process.exit(-1)
   }
 }
